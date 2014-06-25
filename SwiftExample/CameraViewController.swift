141eb0b3d91ddbf26c6fd7d653d3f8efe1ef60f2
Sachin Kesiraju <Sachin@Sachin-Kesiraju-Macbook-Pro.local>
2014-06-25 14:15:11 +0530

    Added camera view.

---
+++ SwiftExample/CameraViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CameraViewController.swift
+//  SwiftExample
+//
+//  Created by Sachin Kesiraju on 6/24/14.
+//  Copyright (c) 2014 Sachin Kesiraju. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UIAlertViewDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var backgroundImage:UIImageView
+    
+    var cameraUI:UIImagePickerController = UIImagePickerController()
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+    
+    init(coder aDecoder: NSCoder!)
+    {
+        super.init(coder: aDecoder)
+    }
+
+    //pragma mark - View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presentCamera()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cameraShow()
+    {
+        self.presentCamera()
+    }
+    
+    //pragma mark - Camera
+    
+    func presentCamera()
+    {
+        cameraUI = UIImagePickerController()
+        cameraUI.delegate = self
+        cameraUI.sourceType = UIImagePickerControllerSourceType.Camera
+        cameraUI.mediaTypes = [kUTTypeImage]
+        cameraUI.allowsEditing = false
+        
+        self.presentViewController(cameraUI, animated: true, completion: nil)
+    }
+    
+    //pragma mark- Image
+    
+    func imagePickerControllerDidCancel(picker:UIImagePickerController)
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker:UIImagePickerController!, didFinishPickingMediaWithInfo info:NSDictionary)
+    {
+        var mediaType:NSString = info.objectForKey(UIImagePickerControllerEditedImage) as NSString
+        var imageToSave:UIImage
+        
+        imageToSave = info.objectForKey(UIImagePickerControllerOriginalImage) as UIImage
+        
+        UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+        self.savedImage()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func savedImage()
+    {
+        var alert:UIAlertView = UIAlertView()
+        alert.title = "Saved!"
+        alert.message = "Your picture was saved to Camera Roll"
+        alert.delegate = self
+        alert.addButtonWithTitle("Awesome")
+        alert.show()
+    }
+    
+    func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int)
+    {
+        NSLog("Did dismiss button: %d", buttonIndex)
+        self.presentCamera()
+    }
+}
