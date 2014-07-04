724f08f85c191fed2b7ae10e6980a4e48c9697b5
Sachin Kesiraju <Sachin@Sachin-Kesiraju-Macbook-Pro.local>
2014-07-04 11:31:37 +0530

    Minor update.

--- SwiftExample/TableViewController.swift
+++ SwiftExample/TableViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class TableViewController: UITableViewController {
 
     var tableArray:NSMutableArray = ["Swift", "Objective -C", "Python", "Java", "Ruby"]
-    var snapshot:UIView = UIView()
-    var sourceIndexPath:NSIndexPath = NSIndexPath()
     
     init(style: UITableViewStyle) {
         super.init(style: style)
@@ -35,58 +33,6 @@ class TableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*@IBAction func longPressRecognized(sender:AnyObject){
-        
-        var longGesture:UILongPressGestureRecognizer = sender as UILongPressGestureRecognizer
-        var gestureState:UIGestureRecognizerState = longGesture.state
-        
-        var point:CGPoint = longGesture.locationInView(self.tableView)
-        var indexPath:NSIndexPath = self.tableView.indexPathForRowAtPoint(point)
-        
-        switch(gestureState){
-            
-        case UIGestureRecognizerState.Began:
-        {
-            
-            if(indexPath)
-            {
-                sourceIndexPath = indexPath
-                var cell:UITableViewCell = self.tableView.cellForRowAtIndexPath(indexPath)
-                snapshot = self.customSnapshotFromView(cell)
-                
-                var center:CGPoint = cell.center
-                snapshot.center = center
-                snapshot.alpha = 0.0
-                self.tableView.addSubview(snapshot)
-                UIView.animateWithDuration(0.25, animations: {(
-                    
-                    center.y = point.y,
-                    snapshot.center = center,
-                    snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05),
-                    snapshot.alpha = 0.98,
-                    
-                    cell.backgroundColor = UIColor.blackColor()
-                    
-                    )})
-            }
-        }
-        break
-        
-    }
-    
-    func customSnapshotFromView(inputView:UIView) -> UIView
-    {
-        var snapshot:UIView = inputView.snapshotViewAfterScreenUpdates(true)
-        snapshot.layer.masksToBounds = false
-        snapshot.layer.cornerRadius = 0.0
-        snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0)
-        snapshot.layer.shadowRadius = 5.0
-        snapshot.layer.shadowOpacity = 0.4
-        
-        return snapshot
-    }
-    
-*/
     //#pragma mark - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView?) -> Int {
@@ -128,35 +74,11 @@ class TableViewController: UITableViewController {
         self.showAlert(tableArray.objectAtIndex(indexPath.row) as NSString, rowToUseInAlert: indexPath.row)
     }
     
-    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool
-    {
-        if(indexPath.row == 0)
-        {
-            return false
-        }
-        else
-        {
-            return true
-        }
-    }
-    
-    override func tableView(tableView: UITableView!, moveRowAtIndexPath sourceIndexPath: NSIndexPath!, toIndexPath destinationIndexPath: NSIndexPath!)
-    {
-        var stringToMove:NSString = self.tableArray.objectAtIndex(sourceIndexPath.row) as NSString
-        self.tableArray.removeObjectAtIndex(sourceIndexPath.row)
-        self.tableArray.insertObject(stringToMove, atIndex: sourceIndexPath.row)
-    }
-    
-    override func tableView(tableView: UITableView!, targetIndexPathForMoveFromRowAtIndexPath sourceIndexPath: NSIndexPath!, toProposedIndexPath proposedDestinationIndexPath: NSIndexPath!) -> NSIndexPath!
-    {
-        return proposedDestinationIndexPath
-    }
-
     //#pragma mark - UIAlertView delegate methods
     
-        func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int) {
-            NSLog("Did dismiss button: %d", buttonIndex)
-        }
+    func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int) {
+        NSLog("Did dismiss button: %d", buttonIndex)
+    }
         
         // Function to init a UIAlertView and show it
     func showAlert(rowTitle:NSString, rowToUseInAlert: Int) {
